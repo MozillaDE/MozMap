4a16ebd7d19c397bb073bc48897167547e534790
Alexander Ploner <alex@neatnet.de>
2015-08-15 20:17:59 +0200

    Implemented Mozillians list that is connected with the map. Added AngularJS. Removed jQuery.

--- static/default.js
+++ static/default.js
@@ -1,81 +1,103 @@
 'use strict';
 
-var blub = [],
-	featureLayer,
-	map;
+angular.module('MozMap', [])
+.controller('MozMapController', function ($scope, $http) {
 
-$.getJSON('./webconf.json', function (json) {
+	$scope.mozillians = [];
+	$scope.activeUser = null;
+	$scope.openPopup = function (user) {
+		if (user.marker) {
+			user.marker.openPopup();
+		} else {
+			$scope.activeUser.marker.closePopup();
+		}
+		$scope.activeUser = user;
+	}
+	
+	$scope.settings = {
+		irc: false
+	};
+	
+	var featureLayer,
+		map;
+	
+	$http.get('./webconf.json').then(function (res) {
+		
+		L.mapbox.accessToken = res.data.mapbox_access_token;
+	
 
-	L.mapbox.accessToken = json.mapbox_access_token;
+		map = L.mapbox.map('map', 'mapbox.streets');
+		featureLayer = new L.mapbox.featureLayer(); // L.MarkerClusterGroup();
 
 
-	map = L.mapbox.map('map', 'mapbox.streets');
-	featureLayer = new L.mapbox.featureLayer(); // L.MarkerClusterGroup();
+		$http.get('./mozillians.json').then(function (res) {
+			var json = res.data;
+			$scope.mozillians = json;
 
-	$.getJSON('./mozillians.json', function (json) {
+			for (var i = 0; i < json.length; i++) {
+				var user = json[i];
 
-		for (var i = 0; i < json.length; i++) {
-
-			var user = json[i],
-				data = user.map_data;
-
-			if (data) {
-
-				showMap(null, data, user);
-
-			} else {
-				console.info(user.username, 'hat keinen Standort angegeben');
+				if (user.map_data) {
+					addMarker(user);
+				}
 			}
 
-		}
-
-		featureLayer.addTo(map);
-		map.fitBounds(featureLayer.getBounds());
-
+			featureLayer.addTo(map);
+			map.fitBounds(featureLayer.getBounds());
+		});
 	});
+
+	function addMarker(user) {
+		
+		user.marker = L.marker([user.map_data.center[1], user.map_data.center[0]], {
+				icon: L.mapbox.marker.icon({
+				  'marker-color': '#C13832'
+				})
+			})
+			.bindPopup(getPopUpHTML(user))
+			.addTo(featureLayer)
+			.on('click', function (e) {
+				$scope.activeUser = user;
+				$scope.$apply();
+			});
+		
+	}
+
+	function getPopUpHTML(user) {
+		return (
+			'<div class="popup_main">' +
+
+				'<img src="' + user.photo + '" alt="Picture" width="50" height="50">' +
+
+				'<div class="text_info">' +
+					// name
+					'<b>' + (user.full_name || user.username) + '</b>' +
+
+					// vouch status
+					(user.is_vouched
+						? '' // ' <i class="fa fa-check" title="Andere Mozillians haben für dieses Profil gebürgt."></i>'
+						: ' <i class="fa fa-exclamation-circle" title="Für dieses Profil wurde noch nicht gebürgt."></i>') +
+
+					'<br>' +
+
+					// irc TODO make this optional
+					($scope.settings.irc && user.ircname
+					    ? ('<i class="fa fa-fw fa-comments" title="IRC"></i> ' + user.ircname + '<br>') : '') +
+
+					// location
+					(user.location ? ('<i class="fa fa-fw fa-map-marker"></i> ' + user.location + '<br>') : '') +
+
+					// profile
+					'<i class="fa fa-fw fa-globe"></i> <a href="' + user.url + '" target="_blank">Mozillians-Profil</a>' +
+
+				'</div>'+
+
+			'</div>' +
+
+			'<div style="clear:both"></div>');
+	}
+
 });
 
-function showMap(err, data, user) {
 
-	var marker = L.marker([data.center[1], data.center[0]], {
-			icon: L.mapbox.marker.icon({
-			  'marker-color': '#C13832'
-			})
-		})
-		.bindPopup(getPopUpHTML(user))
-		.addTo(featureLayer);
-		//.openPopup();
-}
 
-function getPopUpHTML(user) {
-	return (
-		'<div class="popup_main">' +
-
-			'<img src="' + user.photo + '" alt="Gravatar" width="50" height="50">' +
-
-			'<div class="text_info">' +
-				// name
-				'<b>' + (user.full_name || user.username) + '</b>' +
-
-				// vouch status
-				(user.is_vouched // TODO
-					? '' // ' <i class="fa fa-check" title="Andere Mozillians haben für dieses Profil gebürgt."></i>'
-					: ' <i class="fa fa-exclamation-circle" title="Für dieses Profil wurde noch nicht gebürgt."></i>') +
-
-				'<br>' +
-
-				// irc TODO make this optional
-				// (user.ircname ? ('<i class="fa fa-fw fa-comments" title="IRC"></i> ' + user.ircname + '<br>') : '') +
-
-				// location
-				(user.location ? ('<i class="fa fa-fw fa-map-marker"></i> ' + user.location + '<br>') : '') +
-
-				// profile
-				'<i class="fa fa-fw fa-globe"></i> <a href="' + user.url + '" target="_blank">Mozillians-Profil</a>' +
-
-			'</div>'+
-
-		'</div>' +
-
-		'<div style="clear:both"></div>');
-}
